2e67aedcf114eb01da2188484e3197a17c4f1028
Swapnil Awasthi <swapnil28awasthi@gmail.com>
2023-05-08 23:38:46 +0200

    changed the strutcture of the log file

--- reduce_size.v1.py
+++ reduce_size.v1.py
@@ -26,19 +26,15 @@ class Logger(object):
     def log(self, line):
         TODAY_YMD = '_'.join(datetime.datetime.today().strftime("%c").split())
         self.log_fh.write(f"{TODAY_YMD}\t{line}\n")
-    def log_sep(self):
-        self.log_fh.write(f"{'_'*200}\n")
 
 def get_last_dir_and_rest(path):
     rest_path, last_dir = os.path.split(path)
     return rest_path, last_dir
 
-def write_logs(line, print_sep=None):
-    logger_object = Logger(f'{global_path}/reduce_size.v1.log')
-    if print_sep is None:
-        logger_object.log(f"{path}\t{line}")
-    else:
-        logger_object.log_sep()
+def write_logs(line):
+    logger_object = Logger(f'{global_path}/reduce_size.all.logs')
+    logger_object.log(f"{line}")
+
 
 def get_last_dir_and_rest(path):
     rest_path, last_dir = os.path.split(path)
@@ -54,22 +50,22 @@ def check_file_type(path):
     else:
         return 3 
 
-def delete_files(files_to_delete):
+def delete_files(files_to_delete, outlog):
     for file_path in files_to_delete:
         try:
             file_type=check_file_type(file_path)
             if file_type==2:
                 os.remove(file_path)
-                write_logs(f"This file was deleted! : {file_path}")
+                write_logs(f"Deleted_{outlog}\t{file_path}")
             elif file_type==1:
                 shutil.rmtree(file_path)
-                write_logs(f"This directroy was deleted! : {file_path}")
+                write_logs(f"Deleted_{outlog}\t{file_path}")
             elif file_type==0:
-                write_logs(f"This is a symlink so NOT deleted! : {file_path}")
+                write_logs(f"NotDeleted_SymLink_{outlog}\t{file_path}")
         except FileNotFoundError:
-            write_logs(f"This file was not found! : {file_path}")
+            write_logs(f"NotDeleted_NotFound_{outlog}\t{file_path}")
 
-def compress_files(file_paths):
+def compress_files(file_paths, outlog):
     compressed_paths=[]
     for file_path in file_paths:
         file_type=check_file_type(file_path)
@@ -77,40 +73,40 @@ def compress_files(file_paths):
             with tarfile.open(f"{file_path}.tar.gz", "w:gz") as tar:
                 tar.add(file_path, arcname=os.path.basename(file_path))
             compressed_paths.append(f"{file_path}.tar.gz")
-            write_logs(f"This file was successfully compressed! : {file_path}.tar.gz")
+            write_logs(f"Compressed_{outlog}\t{file_path}.tar.gz")
 
         elif file_type==2:
             subprocess.run(["gzip", "-f", "-c", file_path], stdout=open(f"{file_path}.gz", "wb"))
             compressed_paths.append(f"{file_path}.gz")
-            write_logs(f"This file was successfully compressed! : {file_path}.gz")
+            write_logs(f"Compressed_{outlog}\t{file_path}.gz")
         elif file_type==0:
-            write_logs(f"NOT compressed! This is a symlink! : {file_path}")
+            write_logs(f"NotCompressed_SymLink_{outlog}\t{file_path}")
         else:
-            write_logs(f"NOT compressed! This directory doesn't exist! : {file_path}")
+            write_logs(f"NotCompressed_NotFound_{outlog}\t{file_path}")
     return compressed_paths
 
-def validate_compress_files(compressed_paths):
+def validate_compress_files(compressed_paths, outlog):
     validation_results=[True]
     for file_path in compressed_paths:
         if file_path.endswith('.tar.gz'):
             # Validate .tar.gz file
             try:
                 subprocess.check_call(['tar', 'tzf', file_path], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-                write_logs(f"Validation of zipped file was Successfull : {file_path}")
+                write_logs(f"Validated_{outlog}\t{file_path}")
             except subprocess.CalledProcessError:
-                write_logs(f"Validation of this zipped file Failed! : {file_path}")
+                write_logs(f"ValidationFailed_{outlog}\t{file_path}")
                 validation_results.append(False)
         elif file_path.endswith('.gz'):
             # Validate .gz file
             try:
                 subprocess.check_call(['gunzip', '-t', file_path])
-                write_logs(f"Validation of zipped file was Successfull! : {file_path}")
+                write_logs(f"Validated_{outlog}\t{file_path}")
             except subprocess.CalledProcessError:
-                write_logs(f"Validation of zipped file was Failed! : {file_path}")
+                write_logs(f"ValidationFailed_{outlog}\t{file_path}")
                 validation_results.append(False)
         else:
             # Invalid file type
-            write_logs(f"Validation failed: Unknown filetype! : {file_path}")
+            write_logs(f"ValidationFailed_{outlog}\t{file_path}")
             validation_results.append(False)
     return validation_results
 
@@ -127,16 +123,15 @@ def find_empty_files(path):
                     empty_files.append(filepath)
     return empty_files
 
-def compress_validate_delete(path):
-    compressed_paths=compress_files(path)
-    validation_results=validate_compress_files(compressed_paths)
+def compress_validate_delete(path, outlog):
+    compressed_paths=compress_files(path, outlog)
+    validation_results=validate_compress_files(compressed_paths, outlog)
     if all(validation_results):
-        delete_files(path)
-        write_logs("FINISHED!!!")
+        delete_files(path, outlog)
     else:
-        write_logs("FAILED!!!")
+        write_logs(f"NotDeleting_{outlog}")
 
-def reduce_cobg_dir(path):
+def reduce_cobg_dir(path, outlog):
     patern_to_delete=['.bg.fam','.bg.bed','bgs.fam','bgs.bed']
     files_to_delete=[]
     patern_to_compress=['.bg.bim','bgs.bim']
@@ -149,17 +144,16 @@ def reduce_cobg_dir(path):
             files_to_compress.append(d)
 
     if files_to_compress:
-        compressed_paths=compress_files(files_to_compress)
-        validation_results=validate_compress_files(compressed_paths)
+        compressed_paths=compress_files(files_to_compress, outlog)
+        validation_results=validate_compress_files(compressed_paths, outlog)
         
         if all(validation_results):
-            delete_files(files_to_delete)
-            delete_files(files_to_compress)
-            write_logs("FINISHED!!!")
+            delete_files(files_to_delete, outlog)
+            delete_files(files_to_compress, outlog)
         else:
-            write_logs("FAILED!!!")
+            write_logs(f"NotDeleting_{outlog}")
 
-def reduce_tmp_report(path):
+def reduce_tmp_report(path, outlog):
     path = str(path)
     patern_to_delete=['.fam','.bim','.bed'] 
     files_to_delete=[]
@@ -167,10 +161,10 @@ def reduce_tmp_report(path):
         if (d[-4:]) in patern_to_delete:
             files_to_delete.append(d)
 
-    delete_files(files_to_delete)
-    compress_validate_delete([path])
+    delete_files(files_to_delete, outlog)
+    compress_validate_delete([path], outlog)
 
-def reduce_pcaer_sub(path):
+def reduce_pcaer_sub(path, outlog):
     path =str(path)
     patern_to_delete=['.menv.mds.asso-nup.pdf.gz']
     files_to_delete=[]
@@ -178,15 +172,14 @@ def reduce_pcaer_sub(path):
         if (d[-25:]) in patern_to_delete:
             files_to_delete.append(d)
     
-    delete_files(files_to_delete)
-    compress_validate_delete([path])
+    delete_files(files_to_delete, outlog)
+    compress_validate_delete([path], outlog)
 
-def remove_zero_files(path):
+def remove_zero_files(path, outlog):
     empty_files=find_empty_files(path)
     nfiles=len(empty_files)
     if nfiles > 0:
-        delete_files(empty_files)
-        write_logs(f"Deleted {nfiles} file/s from this. FINISHED!!!")
+        delete_files(empty_files,outlog)
 
 def find_files_or_dirs(path, pattern):
     matching_paths = []
@@ -206,13 +199,11 @@ if __name__ == '__main__':
     try:
         if check_file_type(path)!=1:
             global_path=cwd
-            write_logs("Script started!")
-            write_logs("EXITING: This is NOT a directroy or this directroy doesn't EXIST!")
+            write_logs("Not-A-Directory")
             exit()
 
         if check_file_type(path)==1:
             if args.allfunctions:
-                write_logs("Script started!")
                 cob_path=        find_files_or_dirs(path, "^cobg_dir_genome_wide$")
                 tmp_path=        find_files_or_dirs(path, r"^tmp_report_.*\d$")
                 errandout_path=  find_files_or_dirs(path, "^errandout$")
@@ -225,91 +216,78 @@ if __name__ == '__main__':
                 ###Delete all errandout directories 
                 if len(errandout_path)>0:
                     try:
-                        write_logs('_','_')
-                        delete_files(errandout_path)
+                        delete_files(errandout_path, "errandout")
                     except Exception as e:
-                        write_logs(f"ERROR in deleting errandout directroy :{e}")
+                        write_logs(f"ERROR_errandout_{e}")
 
                 ###Delete a files with size 0 excludes symlinks
                 try:
-                    write_logs('_','_')
-                    remove_zero_files(path)
+                    remove_zero_files(path, "size-zero")
                 except Exception as e:
-                    write_logs(f"ERROR in function remove_zero :{e}")
+                    write_logs(f"ERROR_size-zero_{e}")
 
                 ### compress, validate and delete (CVD) dasuqc1_*/info directory 
                 if len(dasuqc1_path)>0:
                     try:
-                        write_logs('_','_')
                         dasuqc1_info_path = [f"{p}/info" for p in dasuqc1_path if check_file_type(p)==1]
-                        compress_validate_delete(dasuqc1_info_path)
+                        compress_validate_delete(dasuqc1_info_path, "dasuqc1/info")
                     except Exception as e:
-                        write_logs(f"ERROR while CVD dasuqc_*/ :{e}")
+                        write_logs(f"ERROR_dasuqc1/info_{e}")
                 
                 ### delete files (*menv.mds.asso.pdf, *mds.asso-nup.pdf.gz and *menv.assomds*qassoc) from pcaer_*  and then CVD
                 if len(pca_qassoc_path)>0:
                     try:
-                        write_logs('_','_')
                         pcaer_sub_path = list(set([os.path.dirname(os.path.dirname(p)) for p in pca_qassoc_path]))
-                        delete_files(pca_qassoc_path+pca_assopdf_path)
+                        delete_files(pca_qassoc_path+pca_assopdf_path, "pcaer-sub")
                         for p in pcaer_sub_path:
-                            write_logs('_','_')
-                            reduce_pcaer_sub(p)
+                            reduce_pcaer_sub(p, "pcaer-sub")
                     except Exception as e:
-                        write_logs(f"ERROR in deleting or CVD from pcaer_* :{e}")
+                        write_logs(f"ERROR_pcaer-sub_{e}")
 
                 ### CVD resdaner directory
                 if len(resdaner_path)>0:
                     try:
-                        write_logs('_','_')
-                        compress_validate_delete(resdaner_path)
+                        compress_validate_delete(resdaner_path, "resdaner")
                     except Exception as e:
-                        write_logs(f"ERROR while CVD resdaner :{e}")
+                        write_logs(f"ERROR_resdaner_{e}")
 
                 ### deleting *assoc.dosage.ngt.gz files and CVD daner_subdirs   
                 if len(assoc_dos_path)>0: 
                     try:
-                        write_logs('_','_')
                         daner_paths = list(set([os.path.dirname(os.path.dirname(p)) for p in assoc_dos_path]))
-                        delete_files(assoc_dos_path)
-                        compress_validate_delete(daner_paths)
+                        delete_files(assoc_dos_path, "daner-sub")
+                        compress_validate_delete(daner_paths, "daner-sub")
                     except Exception as e:
-                        write_logs(f"ERROR in deleting or CVD *assoc.dosage.ngt.gz and daner_subdirs respectively :{e}")
+                        write_logs(f"ERROR_daner-sub_{e}")
                 
                 ### tmp_report
                 if len(tmp_path)>0:
                     try:
                         for p in tmp_path:
-                            write_logs('_','_')
-                            reduce_tmp_report(p)
+                            reduce_tmp_report(p, "tmp-report")
                     except Exception as e:
-                        write_logs(f"ERROR in function reduce_tmp_report :{e}")
+                        write_logs(f"ERROR_tmp-report_{e}")
                 
-                ### cobdg_
+                ### cobg_dir
                 if len(cob_path)>0:
                     try:
                         for p in cob_path:
-                            write_logs('_','_')
-                            reduce_cobg_dir(p)
+                            reduce_cobg_dir(p,"cobg-dir")
                     except Exception as e:
-                        write_logs(f"ERROR in function reduce_cobg_dir :{e}")
+                        write_logs(f"ERROR_cobg-dir_{e}")
             
             elif args.delfiles:
-                write_logs("Script started!")
-                remove_zero_files(path)
+                remove_zero_files(path, 'size-zero')
             
             elif last_dir=="cobg_dir_genome_wide":
-                write_logs("Script started!")
-                reduce_cobg_dir(path)
+                reduce_cobg_dir(path, 'cobg-dir')
             
             elif last_dir[0:11]=="tmp_report_":
                 global_path=rest_path
-                write_logs("Script started!")
-                reduce_tmp_report(path)
+                reduce_tmp_report(path, 'tmp-report')
             
             else:
-                write_logs("Script started!")
-                write_logs("EXITING: This is NOT a cobg_dir_genome_wide or tmp_report_ directroy!")
+                write_logs("Not-A-Valid-Directory")
     
     except Exception as e:
-        write_logs(f"ERROR:{e}")
+        write_logs(f"ERROR_{e}")
