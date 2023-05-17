0334c80f51a586b43ddf580e92d59b85e5c65e79
Swapnil Awasthi <swapnil28awasthi@gmail.com>
2023-05-18 00:03:44 +0200

    deleted files

--- reduce_size.job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Set job requirements
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --partition=shared
-#SBATCH --time=01:00:00
- 
-python3 /home/pgca1mdd/reduce_size/reduce_size_ricopili/reduce_size.v1.py --dir /home/pgca1mdd/GSRD/ --all_actions
